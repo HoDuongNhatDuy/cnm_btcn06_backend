620a2fed4399db9a95eeb9ffa357b510b370ef50
nhatduy <nhatduy1tn@gmail.com>
2017-12-26 23:09:16 +0700

    demo use async/await function

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -20,38 +20,80 @@ exports.GetWallets = function (req, res, next) {
         });
 };
 
-exports.GetTotalInfo = function (req, res, next) {
-    let user_id = req.params.id;
-    let user    = null;
-    let wallets = null;
+function findUser(userId) {
+    return new Promise(resolve => {
+        User.findOne({_id: userId}, function (error, user_result) {
+                console.log("123", user_result);
+                if (!user_result) {
+                    resolve(null);
+                }
+                else {
+                    resolve(user_result);
+                }
+            })
+    });
+}
 
-    User.findOne({_id: user_id}).exec()
-        .then(function (user_result) {
-            if (!user_result) {
-                res.json({
-                    status: 0,
-                    message: "User not found",
-                });
-                return null;
-            }
-
-            user = user_result;
-            return Wallet.find().where({user: user_id}).exec();
-        })
-        .then(function (wallets_result) {
-            wallets = wallets_result;
-
-            return Transaction.find().where({$or: [{source_user: user_id}, {dest_user: user_id}]}).limit(5).sort({created_at: -1}).populate(["source_wallet", "source_user", "dest_wallet"]).exec()
-        })
-        .then(function (transactions) {
-            result = {
-                transactions: transactions,
-                wallets: wallets
-            };
-            res.json({
-                status: 1,
-                message: "Got total info successfully",
-                data: result
+function getWalletsByUserId(userId) {
+    return new Promise(resolve => {
+        Wallet.find({user: userId}, function (error, wallets_result) {
+                if (!wallets_result) {
+                    resolve([]);
+                }
+                else {
+                    resolve(wallets_result);
+                }
             });
+    });
+}
+
+function getTransactionByUserId(userId) {
+    return new Promise(resolve => {
+        Transaction.find({$or: [{source_user: userId}, {dest_user: userId}]}).limit(5).sort({created_at: -1}).populate(["source_wallet", "source_user", "dest_wallet"]).exec(function (error, transactions) {
+                if (!transactions) {
+                    resolve([]);
+                }
+                else {
+                    resolve(transactions);
+                }
+            });
+    });
+}
+
+exports.GetTotalInfo = async function (req, res, next) {
+    try {
+        let userId = req.params.id;
+        user         = await findUser(userId);
+
+        if (!user) {
+            res.json({
+                status: 0,
+                message: 'User not found'
+            });
+        }
+
+        // call these 2 function at the same time
+        let wallets      = getWalletsByUserId(userId);
+        let transactions = getTransactionByUserId(userId);
+
+        // wait for them
+        await wallets;
+        await transactions;
+
+        result = {
+            transactions: transactions,
+            wallets: wallets
+        };
+        res.json({
+            status: 1,
+            message: "Got total info successfully",
+            data: result
         });
+    }
+    catch (e) {
+        res.json({
+            status: 0,
+            message: e
+        });
+    }
 };
